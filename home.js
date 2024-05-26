154636f61727991324252bd2ca7a1194bc4efd0e
Avotexs <abderahmanedoubabi@gmail.com>
2024-05-26 15:03:51 +0100

    intial comit

---
+++ home.js
@@ -0,0 +1,9 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+
+function car(voiture){
+    img.src = voiture;
+}
+function voi_img(x){
+    container.style.background = x;
+}
